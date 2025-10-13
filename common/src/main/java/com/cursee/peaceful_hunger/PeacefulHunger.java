6c002ffb3d11fa74f5cf63b846d0fb8ab9cc3d06
jason13official <jason13official@gmail.com>
2025-10-13 13:08:54 -0400

    load config later

--- common/src/main/java/com/cursee/peaceful_hunger/PeacefulHunger.java
+++ common/src/main/java/com/cursee/peaceful_hunger/PeacefulHunger.java
@@ -1,10 +1,12 @@
 package com.cursee.peaceful_hunger;
 
+import com.cursee.peaceful_hunger.impl.common.config.PeacefulHungerConfig;
 import net.minecraft.resources.ResourceLocation;
 
 public class PeacefulHunger {
 
   public static void init() {
+    new PeacefulHungerConfig().onLoad();
   }
 
   public static ResourceLocation identifier(String path) {
